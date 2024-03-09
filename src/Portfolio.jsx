55257201393be2874b5bc98b4f59379891179285
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-03-09 00:28:08 -0600

    Actualiza estilos y agrega barra de navegación a la página de portafolio

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,12 +1,29 @@
-import styles from './App.module.css'
+import styles from './Portfolio.module.css';
 
 function Portfolio() {
-
   return (
     <div>
-      <h1>React App</h1>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand d-flex align-items-center" href="#">
+            <img src="/portatil.png" alt="" width="50" height="45" className="d-inline-block align-text-top" />
+            NoeOnDev
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav ms-auto">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <a className="nav-link" href="#">Features</a>
+              <a className="nav-link" href="#">Pricing</a>
+              <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
