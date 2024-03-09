d1f986d14516bbed114c013ad626b52502145965
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-03-09 01:20:28 -0600

    Actualización de estilos y navegación en el portafolio

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,11 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import styles from './Portfolio.module.css';
+import './Custom.css';
 
 function Portfolio() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand d-flex align-items-center" href="#">
             <img src="/portatil.png" alt="" width="50" height="45" className="d-inline-block align-text-top" />
@@ -16,9 +17,10 @@ function Portfolio() {
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav ms-auto">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
-              <a className="nav-link active" aria-current="page" href="#">Features</a>
-              <a className="nav-link active" aria-current="page" href="#">Pricing</a>
+              <a className="nav-link active" aria-current="page" href="#">About</a>
+              <a className="nav-link active" aria-current="page" href="#">Resume</a>
+              <a className="nav-link active" aria-current="page" href="#">Portfolio</a>
+              <a className="nav-link active" aria-current="page" href="#">Contact</a>
             </div>
           </div>
         </div>
