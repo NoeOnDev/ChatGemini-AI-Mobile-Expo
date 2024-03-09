7822fd1b2995b303f73fb62e17c19d7f2e1e3079
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-03-09 13:06:14 -0600

    Agregadas dependencias de tipos para Bootstrap y corregido el espaciado en la barra de navegación

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -9,18 +9,18 @@ function Portfolio() {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand d-flex align-items-center" href="#">
-            <img src="/portatil.png" alt="" width="50" height="45" className="d-inline-block align-text-top" />
+            <img src="/portatil.png" alt="" width="50" height="45" className="d-inline-block align-text-top me-2" />
             NoeOnDev
           </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav ms-auto">
-              <a className="nav-link active" aria-current="page" href="#">About</a>
-              <a className="nav-link active" aria-current="page" href="#">Resume</a>
-              <a className="nav-link active" aria-current="page" href="#">Portfolio</a>
-              <a className="nav-link active" aria-current="page" href="#">Contact</a>
+          <div className="navbar-nav ms-auto">
+              <a className="nav-link active me-4" aria-current="page" href="#">About</a>
+              <a className="nav-link active me-4" aria-current="page" href="#">Resume</a>
+              <a className="nav-link active me-4" aria-current="page" href="#">Portfolio</a>
+              <a className="nav-link active me-4" aria-current="page" href="#">Contact</a>
             </div>
           </div>
         </div>
