2521e9fed60d53b7bebcff68201d68c64d357024
NoeOnDev <alxg5516@gmail.com>
2024-07-05 00:18:00 -0600

    Actualizar margen derecho del perfil y tamaño de fuente de párrafo

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Portfolio from './Portfolio'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Portfolio />
-  </React.StrictMode>,
-)
