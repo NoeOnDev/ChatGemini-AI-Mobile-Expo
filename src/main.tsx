d3b0707eec786247fbd3dd9c6dfca6f26499edfe
NoeOnDev <alxg5516@gmail.com>
2024-07-25 11:45:25 -0600

    Eliminar archivos y código innecesarios, actualizar estilos y dependencias, y agregar componentes HomePage y App

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import './styles/main.css';
-import { App } from "./containers/App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
