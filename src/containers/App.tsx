5e27b3ff285b664aa121f014b59848d1eafa57c2
NoeOnDev <alxg5516@gmail.com>
2024-07-25 12:19:01 -0600

    Actualizar importación de HomePage en App.tsx y eliminar importación de index.css

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomePage } from "../components/pages/HomePage";
+import { HomePage } from "../pages/HomePage";
 
 export const App = () => {
   return (
